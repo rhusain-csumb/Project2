0f8e963048270c043c3c5224f419ee7bd40d9666
Chanroop Randhawa <chanrooprandhawa@gmail.com>
2025-08-05 16:07:40 -0700

    created the Login.java as well as the UI and the XML file for it.

--- app/src/main/java/com/example/PetPal/model/Pet.java
+++ app/src/main/java/com/example/PetPal/model/Pet.java
@@ -1,5 +1,5 @@
 /**
- * This is an andriod moble application called PetPal. This practical app will track pet visits,
+ * This is an android moble application called PetPal. This practical app will track pet visits,
  * vaccinations, feeding schedules, and medications. Key features will include an emergency contact list
  * and the potential to scan food/medication for streamlined data entry.
  * @authors: Rasna Husain and Chanroop Randhawa
