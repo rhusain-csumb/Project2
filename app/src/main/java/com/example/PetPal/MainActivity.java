cc3bd6e56c332d0d82863ad7da3097d13a68010a
Rasna <rasna.rh@gmail.com>
2025-08-13 03:38:56 -0700

    Added MainActivity.java and activity_main.xml

---
+++ app/src/main/java/com/example/PetPal/MainActivity.java
@@ -0,0 +1,37 @@
+/**
+ * This is an android moble application called PetPal. This practical app will track pet visits,
+ * vaccinations, feeding schedules, and medications. Key features will include an emergency contact list
+ * and the potential to scan food/medication for streamlined data entry.
+ * @authors: Rasna Husain and Chanroop Randhawa
+ */
+
+package com.example.PetPal;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Start the LoginActivity after a short delay
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //startActivity(LoginActivity.newIntent(MainActivity.this));
+            finish();
+        }).start();
+    }
+}
