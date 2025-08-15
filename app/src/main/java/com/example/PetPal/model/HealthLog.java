addb430bf96e647215bae47aeff1dc6443f50758
rhusain-csumb <rhusain@csumb.edu>
2025-08-14 19:10:28 -0700

    Update HealthLog.java

--- app/src/main/java/com/example/PetPal/model/HealthLog.java
+++ app/src/main/java/com/example/PetPal/model/HealthLog.java
@@ -3,6 +3,7 @@
  * vaccinations, feeding schedules, and medications. Key features will include an emergency contact list
  * and the potential to scan food/medication for streamlined data entry.
  * @authors: Rasna Husain and Chanroop Randhawa
+ 
  */
 
 package com.example.PetPal.model;
